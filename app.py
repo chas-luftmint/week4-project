a2daa615e4b97bee7570256c4cd570c2d3cd33ba
chas-luftmint <armin.ozkalp@chasacademy.se>
2026-02-11 12:18:27 +0100

    added buttons to app.py

--- app.py
+++ app.py
@@ -1,14 +1,26 @@
 # Importing flask class from the flask module like this:
-from flask import Flask
+from flask import Flask, render_template, request
 
 # Instance creation of flask class
 app = Flask(__name__)
 
 # Route defining and function to handle requests to specific route
 @app.route('/')
-def this_works():
-    return 'This works! Wow!'
+def main_page():
+    return '''
+    <h1>Main Page</h1>
+    <p>Main Page</p>
+    <a href="/working"><button>This works?</button></a>
+    '''
 
+@app.route('/working')
+def this_works():
+    return '''
+    <h1>This works</h1>
+    <p>This is a test button, hopefully</p>
+    <a href="/"><button>This should send you somewhere!</button></a>
+    '''
+    
 # Run the app
 if __name__ == '__main__':
     app.run(debug=True)
